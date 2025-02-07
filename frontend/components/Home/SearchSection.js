e69485cda36418e4ad93b8df073aff2efa65a9f0
Sinthorn <psinthorn@gmail.com>
2025-02-07 07:59:43 +0700

    animate individual icon

--- frontend/components/Home/SearchSection.js
+++ frontend/components/Home/SearchSection.js
@@ -6,7 +6,8 @@ import SourceContext from '@/context/SourceContext';
 import DestinationContext from '@/context/DestinationContext';
 import CarListOptions from '../vehicle/CarListOptions';
 import Services from '../services/Services';
-import { CircleArrowDown, CircleChevronDown, Heart, Plane, Smile, SmilePlus, Snowflake, Sun, TreePalm, Waves } from 'lucide-react';
+import { Loader, CircleChevronDown, Heart, Plane, Smile, SmilePlus, Snowflake, Sun, TreePalm, Waves, Car } from 'lucide-react';
+import IconAnimate from '../utilities/IconAnimate';
 
 const SearchSection = () => {
   const {source, setSource} = useContext(SourceContext);
@@ -52,42 +53,24 @@ const SearchSection = () => {
 
   return (
     <div className='space-y-4 p-4 bg-white rounded-none h-full md:p-6'>
-      <div className='p-4 md:p-6 border-2 rounded-t-xl rounded-b-none'>
+        <div className='p-4 md:p-6 border-2 rounded-t-xl rounded-b-none'>
           <p className='text-2xl text-orange-600 font-bold mb-2 sm:text-1xl'>Book Your Transfer Now</p>
           <p>Arrive in Koh Samui. Click below to get an instant quote and secure your ride today!</p>
           <p className='text-2xl font-light mt-4 sm:text-1xl'>
               From where to where? Let us know youre route.
           </p>
-          {/* <p>
-              to check availibility and fare rate now.
-          </p> */}
           <InputItem type='source' />
-          <InputItem type='destination' />    
-          {/* <button 
-            className='w-full p-3 mt-5 bg-orange-400 text-white rounded-lg' 
-            onClick={()=>calculateDistance()}
-          >Search</button>   */}
-      </div>
-      <div className=''>
+          <InputItem type='destination' /> 
+        </div>
+        <div className=''>
             <div className='w-full text-2xl font-light items-center'>
               {routeDistance ? 
                   <div>
                   <h2 className='font-semibold text-2xl text-green-800 pt-8'>Available Book Now</h2>
                   <p className='text-sm text-muted-foreground'>Distance: <span>{ routeDistanceInKiloMeter.toFixed(2)}</span>KM</p>
                   </div>
-              // <>
-              //   <span className='pt-4 pb-0 md:pt-5 md:pb-0  text-green-700 font-bold mt-4'>
-              //     Available Book Now
-              //   </span> 
-              //   <span className='text-sm'>Distance: <span>{ routeDistanceInKiloMeter.toFixed(2)}</span>KM</span> 
-              // </>
               : 
-              <span className='flex justify-center text-center items-center text-slate-400 gap-3'>
-                <Snowflake/><Smile/><Plane/><Sun/><TreePalm/><Waves/><Heart/><SmilePlus/>
-              </span>
-              
-              // <p className="items-center text-center font-bold text-orange-500">" Transfers Made Simple"</p>
-              // <Services />
+                <IconAnimate />
               }
             </div>
         </div>
